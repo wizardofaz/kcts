d7ecffc50b4cb3a416100815a3de9d3a50313bf8
David Freese <w1hkj@w1hkj.com>
2014-06-27 20:14:57 -0500

    Build fixes

--- scripts/build4mingw.sh
+++ scripts/build4mingw.sh
@@ -2,9 +2,17 @@
 
 ### Script to build a win32 installation
 
-./configure $CROSSCFG $PKGCFG FLTK_CONFIG=$PREFIX/bin/fltk-config --with-ptw32=$PREFIX/ptw32 XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config
+./configure \
+  $PKGCFG \
+  $CROSSCFG \
+  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
+  XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config \
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
+
 make
-i586-mingw32msvc-strip src/kcts.exe
+
+$PREFIX/bin/i686-pc-mingw32-strip src/kcts.exe
 make nsisinst
 mv src/*setup*exe .
 
--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -7,9 +7,17 @@ tar czf kcts-$1.bin.tgz -C /tmp/kcts-build/bin kcts
 
 make clean
 
-./configure $CROSSCFG $PKGCFG FLTK_CONFIG=$PREFIX/bin/fltk-config --with-ptw32=$PREFIX/ptw32 XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config
+./configure \
+  $PKGCFG \
+  $CROSSCFG \
+  --with-ptw32=/opt/mxe/usr/i686-pc-mingw32 \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
+  XMLRPC_C_CONFIG=$PREFIX/bin/xmlrpc-c-config \
+  FLTK_CONFIG=$PREFIX/bin/i686-pc-mingw32-fltk-config
+
 make
-i586-mingw32msvc-strip src/kcts.exe
+
+$PREFIX/bin/i686-pc-mingw32-strip src/kcts.exe
 make nsisinst
 mv src/*setup*exe .
 
